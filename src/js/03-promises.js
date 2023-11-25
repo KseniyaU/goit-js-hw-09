22c5c5bb60aa3b3a361d5c9c31c3c32c474adf08
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2023-11-25 11:56:20 +0200

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+//npm i notiflix
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name="delay"]')//перша затримка в мілісекундах
+const step = document.querySelector('input[name="step"]')//крок збільшення затримки після першого промісу
+const amout = document.querySelector('input[name="amount"]')//кількість промісів яку необхідно створити
+const btn = document.querySelector('button')//кнопка відправки
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
+    Notiflix.Notify.success("ok")
     // Fulfill
   } else {
+    Notiflix.Notify.failure("be")
     // Reject
   }
 }
+btn.addEventListener('click', () => {
+  const intervalId = setInterval(() => {
+    for (let i = 0; i < amout.value; i++){
+      createPromise(i+1, delay.value + i*step)
+    }
+  }, delay.value)
+})
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
