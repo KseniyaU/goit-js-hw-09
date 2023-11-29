332f7d518ee5b4373e6f92b876cc81807f7a4574
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2023-11-29 22:13:18 +0200

    task - 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,34 +1,44 @@
 //npm i notiflix
-import Notiflix from 'notiflix';
+import Notiflix from 'notiflix'; //бібліотека для виводу красивих повідомлень
 
-const delay = document.querySelector('input[name="delay"]')//перша затримка в мілісекундах
-const step = document.querySelector('input[name="step"]')//крок збільшення затримки після першого промісу
-const amout = document.querySelector('input[name="amount"]')//кількість промісів яку необхідно створити
-const btn = document.querySelector('button')//кнопка відправки
+const form = document.querySelector('.form')
+// відслуховуємо відправку форми
+form.addEventListener("submit", hendlerSubmi);
+
+function hendlerSubmi(event) {
+  event.preventDefault();
+  const {delay, step, amount} = event.currentTarget.elements;//робимо деструктирізацію полів з форми оскільки вони задані чере name
+  //робимо затримку промісів
+  setTimeout(() => {
+    for (let i = 1; i <= +amount.value; i++){
+      createPromise(i, (step.value * i))
+        .then(data => Notiflix.Notify.success(data))//виводимо позитивний проміс 
+        .catch(arror => Notiflix.Notify.failure(arror))//виводимо негативний проміс
+    }
+  }, +delay.value)
+}
 
 
+
+//функція створення самого промісу
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.success("ok")
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;// рандомне значення
+      //по рандомному значенню викликаємо написи на промісах
+      if (shouldResolve) {
+         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        
     // Fulfill
-  } else {
-    Notiflix.Notify.failure("be")
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+      
     // Reject
   }
-}
-btn.addEventListener('click', () => {
-  const intervalId = setInterval(() => {
-    for (let i = 0; i < amout.value; i++){
-      createPromise(i+1, delay.value + i*step)
-    }
-  }, delay.value)
-})
+    }, delay)
+  }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+  )
+  // 
+ 
+}
